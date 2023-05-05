6e7426fa221f8c127cf3fd258b52e2f801188943
hector-vs <hectorvieirasaldivar@gmail.com>
2023-05-04 21:03:22 -0300

    Mais updates (começo php)

---
+++ assets/php/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favicon/site.webmanifest">
+</head>
+<body id="body-index">
+    <main id="main-login-area">
+        <div id="main-login">
+            <img src="../img/logo-icon-branco.png" alt="Logo" height="80px">
+            <form action="" method="POST">
+                <input type="email" name="email" id="email" autocomplete="off" placeholder="E-mail">
+                <input type="password" name="senha" id="senha" placeholder="Senha">
+                <input type="submit" value="Entrar" id="inputenviar">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
